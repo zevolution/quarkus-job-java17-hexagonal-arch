98c5c4e7b3ddbcba1cd9e848adc40466d7755e0a
zevolution <jose122black@gmail.com>
2023-04-13 12:37:04 -0300

    improve injection test with method validator

--- src/test/java/dev/zevolution/pocquarkusjob/architecture/InjectionTest.java
+++ src/test/java/dev/zevolution/pocquarkusjob/architecture/InjectionTest.java
@@ -1,6 +1,6 @@
 package dev.zevolution.pocquarkusjob.architecture;
 
-import com.tngtech.archunit.core.domain.JavaField;
+import com.tngtech.archunit.core.domain.JavaMember;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchCondition;
@@ -14,11 +14,16 @@ public class InjectionTest {
     @ArchTest
     static ArchRule injectAnnotationFieldValidator = ArchRuleDefinition
             .noFields().should(beAnnotatedWithAnInjectionAnnotation())
-            .because("field injection is considered harmful; use constructor injection or setter injection instead; see https://stackoverflow.com/q/39890849 for detailed explanations");
+            .because("field injection is considered harmful; use constructor injection instead; see https://stackoverflow.com/q/39890849 for detailed explanations");
 
-    private static ArchCondition<JavaField> beAnnotatedWithAnInjectionAnnotation() {
-        ArchCondition<JavaField> annotatedWithSpringAutowired = ArchConditions.beAnnotatedWith("org.springframework.beans.factory.annotation.Autowired");
-        ArchCondition<JavaField> annotatedWithJakartaInject = ArchConditions.beAnnotatedWith("javax.inject.Inject");
+    @ArchTest
+    static ArchRule injectAnnotationMethodValidator = ArchRuleDefinition
+            .noMethods().should(beAnnotatedWithAnInjectionAnnotation())
+            .because("method injection is considered harmful; use constructor injection instead; see https://stackoverflow.com/q/39890849 for detailed explanations");
+
+    private static <T extends JavaMember> ArchCondition<T> beAnnotatedWithAnInjectionAnnotation() {
+        ArchCondition<T> annotatedWithSpringAutowired = ArchConditions.beAnnotatedWith("org.springframework.beans.factory.annotation.Autowired");
+        ArchCondition<T> annotatedWithJakartaInject = ArchConditions.beAnnotatedWith("javax.inject.Inject");
         return annotatedWithSpringAutowired.or(annotatedWithJakartaInject);
     }
 
